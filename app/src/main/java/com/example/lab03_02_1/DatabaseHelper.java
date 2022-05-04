9604976f4b01dea86e50b834919ff84ae906da3c
Duy <nnkhuongduy@gmail.com>
2022-05-05 04:27:37 +0700

    update

---
+++ app/src/main/java/com/example/lab03_02_1/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.example.lab03_02_1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_CREATE = "create table users (_id integer primary key autoincrement, "
+            + "name text not null);";
+
+
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+    }
+}
